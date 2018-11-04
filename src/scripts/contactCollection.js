9e70c4785ddef583e49659debf9d1b042fcec129
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-11-04 11:43:17 -0600

    finally got contacts to post and display

--- src/scripts/contactCollection.js
+++ src/scripts/contactCollection.js
@@ -1,25 +1,18 @@
 // A ContactCollection component that loads existing contacts from storage, and saves new ones. Each new contact should have an auto-generated identifier.
 
 let contactCollection = {
-  get: {
-    value: (resource, id) => {
-      return fetch("http://localhost:8088/entries")
-        .then(result => result.json())
-    }
+  getContact() {
+    return fetch("http://localhost:8088/entries")
+      .then((response) => response.json())
   },
-
-  add: {
-    value: (resource, item) => {
-      return fetch("http://localhost:8088/entries", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(item)
-        })
-        .then(result => result.json())
-    }
+  addContact(savedContact) {
+    return fetch("http://localhost:8088/entries", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(savedContact)
+      })
   }
 }
-
 export default contactCollection
